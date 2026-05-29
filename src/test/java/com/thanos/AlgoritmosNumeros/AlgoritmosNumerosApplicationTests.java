5c9816883b577d7d3a4bf399ad21288f8496c109
polikarmio-crypto <polikarmio3@gmail.com>
2026-05-29 14:04:50 -0400

    Primer commit

---
+++ src/test/java/com/thanos/AlgoritmosNumeros/AlgoritmosNumerosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanos.AlgoritmosNumeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmosNumerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
